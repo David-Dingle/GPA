bd28d1f0b576cd421e11570cdff2e459e0228c53
David-Dingle <xding8@ucmerced.edu>
2024-11-30 17:06:57 -0800

    submodule fix deadlock bug; update ./bin/install.sh ./bin/gpa

--- gpa/torch-monitor/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xjding/Projects/GPA/GPA/torch-monitor
-# Build directory: /home/xjding/Projects/GPA/GPA/gpa/torch-monitor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("driver")
-subdirs("test")
